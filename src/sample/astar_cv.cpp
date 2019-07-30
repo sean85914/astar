a6a8ae4e2fa9bfe84fab6feaaf934dbd7c1c408e
sean85914 <sean19960914@gmail.com>
2019-07-30 20:39:02 +0800

    output video, better visualization

--- src/sample/astar_cv.cpp
+++ src/sample/astar_cv.cpp
@@ -4,24 +4,33 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
 #include "astar.hpp"
 
-int height, width; // Image dimension
+// ./astar_cv height width p
+// It will output a video "demo.avi" in your current directory
 
-static int count = 0;
-int start_x, start_y, end_x, end_y;
 bool get_in = false;
 bool ASTAR_VERBOSE = true;
-
+int height, width; // Image dimension
+int start_x, start_y, end_x, end_y;
+static int count = 0;
+const int FPS = 30;
+cv::Mat img;
 void mouse_cb(int Event, int x, int y, int flags, void* param)
 {
-  
   if(Event == CV_EVENT_LBUTTONUP){
     if(count==0){
+      if(img.at<cv::Vec3b>(y, x) == cv::Vec3b(0., 0., 0.)){
+        std::cerr << "Target on obstacle, ignore...\n"; return;
+      }
       start_x = y; start_y = x; ++count;
       printf("Start point set at (%d, %d)\n", y, x);
     }
     else{
+      if(img.at<cv::Vec3b>(y, x) == cv::Vec3b(0., 0., 0.)){
+        std::cerr << "Target on obstacle, ignore...\n"; return;
+      }
       end_x = y; end_y = x;
       printf("End point set at (%d, %d)\n", y, x);
       get_in = true;
@@ -31,16 +40,18 @@ void mouse_cb(int Event, int x, int y, int flags, void* param)
 
 int main(int argc, char** argv)
 {
-  if(argc!=3) {printf("Not enough inputs.\n"); return 0;}
+  if(argc!=4) {printf("Not enough inputs.\n"); return 0;}
   height = atoi(argv[1]);
   width = atoi(argv[2]);
+  double p = atof(argv[3]);
   
   srand(time(NULL));
-  cv::Mat img(height, width, CV_8UC3);
+  img.create(height, width, CV_8UC3);
+  cv::VideoWriter video("demo.avi", CV_FOURCC('M', 'J', 'P', 'G'), FPS, cv::Size(height, width));
   for(int i=0; i<img.rows; ++i){
     for(int j=0; j<img.cols; ++j){
       double x = (double) rand() / (RAND_MAX +1.0);
-      if(x>=0.2){
+      if(x>=p){
         img.at<cv::Vec3b>(i, j)[0] = 255.;
         img.at<cv::Vec3b>(i, j)[1] = 255.;
         img.at<cv::Vec3b>(i, j)[2] = 255.;
@@ -61,15 +72,18 @@ int main(int argc, char** argv)
     }
   }
   cv::namedWindow("Test", CV_WINDOW_NORMAL);
-  //cv::resizeWindow("Test");
+  cv::resizeWindow("Test", 2*height, 2*width);
   cv::setMouseCallback("Test", mouse_cb, NULL);
   cv::imshow("Test", img);
   cv::waitKey(0);
   while(!get_in) {} // Wait to get information
+  img.at<cv::Vec3b>(start_x, start_y) = cv::Vec3b(0., 255., 0.); // G
+  img.at<cv::Vec3b>(end_x, end_y) = cv::Vec3b(0., 255., 0.); // G
+  cv::circle(img, cv::Point(start_y, start_x), 8, cv::Vec3b(0., 255., 0.), 1);
+  cv::circle(img, cv::Point(end_y, end_x), 8, cv::Vec3b(0., 255., 0.), 1);
+  for(int i=0; i<30; ++i) video << img;
   AStar::Node start(start_x, start_y, NULL), end(end_x, end_y, NULL);
   AStar::AStar planner(start, end, map);
-  printf("Start: (%.f, %.f) with value %.f\n", start.get_x(), start.get_y(), map(start.get_x(), start.get_y()));
-  printf("End: (%.f, %.f) with value %.f\n", end.get_x(), end.get_y(), map(end.get_x(), end.get_y()));
   AStar::NODE_LIST res; // Planned path placeholder
   if(!planner.plan(res)) {printf("Cannot find path.\n"); return 0;}
   printf("%d\n", (int)res.size());
@@ -77,9 +91,11 @@ int main(int argc, char** argv)
     img.at<cv::Vec3b>(it->get_x(), it->get_y())[0] = 0.; // B
     img.at<cv::Vec3b>(it->get_x(), it->get_y())[1] = 0.; // G
     img.at<cv::Vec3b>(it->get_x(), it->get_y())[2] = 255.; // R
-    cv::destroyWindow("Test");cv::namedWindow("Test", CV_WINDOW_NORMAL);
-    cv::imshow("Test", img);
-    if(it!=std::prev(res.end())) cv::waitKey(100); else cv::waitKey(0);
+    video << img;
+    //cv::destroyWindow("Test");cv::namedWindow("Test", CV_WINDOW_NORMAL); cv::resizeWindow("Test", 2*height, 2*width);
+    //cv::imshow("Test", img);
+    //if(it!=std::prev(res.end())) cv::waitKey(1000/FPS); else cv::waitKey(0);
   }
+  
   return 0;
 }
